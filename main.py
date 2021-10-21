5c21b6d850e71442970dbdf47424dfdb76207525
meliudas <batyanite@gmail.com>
2021-10-21 18:48:53 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# 1)
+# insert = [1, 2, 3, 4, 5, 'string', [1,2,], 2,3,]
+#
+# if name == 'main':
+#     # print_hi('PyCharm')
+#     insert. extend('HELLO !')
+#     print(insert)
+
+
+
+  # 2) добовляет буквы которые записал
+# array = ['n', 'a', 'd', 'r']
+# array. append('Л,У, Ч, Ш, И, ,Й,')
+# array.insert(3, 'i')
+#
+# print('List:', array)
+
+
+
+  # 3) убирает букву которую записал
+# array = ['n', 'a', 'd', 'r']
+# array.remove('d')
+# print('Updated List:', array)
+
+
+
+  # 4) убирает то число которое записал
+# array = ['1', '2', '3', '4']
+# array.pop(3)
+# print('Updated List:', array)
+
+
+
+  # 5)
+# array = ['1', '2', '3', '4']
+# array.pop(3)
+# print('Updated List: ', array)
+# print(array.index('3'))
+
+
+  # 6) убирает похожих
+# numbers = [2, 3, 5, 2, 12, 2,]
+# array = numbers.count(2)
+# print('Count of 2:', array)
+
+
+
+  # 7) по норостанию
+# array = [11, 3, 7, 5, 2]
+# array.sort()
+# print(array)
+
+
+
+  # 8) просто капирует
+# numbers = [5, 8, 20]
+# array = numbers.copy()
+# print('Copied List:', array)
+
+
+
+  # 9) показывает какие скобки использованы
+# array = [2, 3, 5, 7, 9, 11]
+# array.clear()
+# print('List after clear:()', array)
